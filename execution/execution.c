fe589d0f74f3546173277e38bb95f61f59479c6c
Khaoula Naboussi <knabouss@e2r8p13.1337.ma>
2021-02-27 11:29:22 +0100

    test

--- execution/execution.c
+++ execution/execution.c
@@ -1,38 +1,14 @@
 
 #include "../headers/minishell.h"
 
-typedef	struct	s_struct
-{
-	char				*cmd;
-	char				*option;
-	char				*argument;
-	struct	s_struct	*next;
-}				t_struct;
-
-t_struct	*init_struct(t_struct *gnrl)
-{
-	gnrl->cmd = "echo";
-	gnrl->option = "-n";
-	gnrl->argument = "hello world";
-	gnrl->next = NULL;
-}
-
 int main(void)
 {
-	char		*buff;
-	t_struct	gnrl;
-	int r = 0;
+	char	*buff;
 
 	while (1)
 	{
-		&gnrl = init_struct(&gnrl);
-		/* ft_putstr_fd("minishell ~>  $",fd);*/
-		if ((r = ft_strncmp(gnrl->cmd, "echo", 4)) == 0)
-		{
-			ft_putstr_fd(gnrl->argument, 1);
-			if (gnrl->option)
-				ft_putchar_fd('\n', 1);
-		}
+		ft_putstr_fd("minishell ~>  $",1);
+		get_next_line(0, &buff);
 	}
 	return (0);
 }
