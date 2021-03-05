0a0501d70d61ff589756c0f923b761dbee06ea24
Khaoula Naboussi <knabouss@e1r13p3.1337.ma>
2021-03-05 10:32:29 +0100

    update

--- execution/execution.c
+++ execution/execution.c
@@ -1,16 +1,1 @@
 
-#include "../headers/minishell.h"
-
-int main()
-{
-	int r = 1;
-	char *buff;
-
-	while (r > 0)
-	{
-		ft_putstr_fd("minishell ~>  $",1);
-		// while()
-		r = read(0, buff, 10);
-		printf("%s", buff);
-	}
-}
