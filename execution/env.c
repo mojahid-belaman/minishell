069e8f0dd6ab1698d5b467fab317c319df3d0c2c
Khaoula Naboussi <knabouss@e2r8p13.1337.ma>
2021-03-09 07:58:42 +0100

    update

--- execution/env.c
+++ execution/env.c
@@ -12,27 +12,38 @@ char	**split_env(char *line)
 	char **key_value;
 
 	key_value = ft_split(line, '=');
-
-		ft_putstr_fd("test\n", 1);
 	return(key_value);
 }
 
-t_env	*create_node(char **key_value)
+t_env	*create_node(t_env *node, char **key_value)
 {
 	t_env	*list;
 	int i;
 
 	i = -1;
 	list = (t_env *)malloc(sizeof(t_env));
-	list->key = (char *)malloc(sizeof(key_value[0]));
-	list->value = (char *)malloc(sizeof(key_value[1]));
-	while(key_value[0][++i])
-		list->key[i] = key_value[0][i];
-	i = -1;
-	while(key_value[1][++i])
-		list->value[i] = key_value[1][i];
+	list->key = ft_strdup(key_value[0]);
+	list->value = ft_strdup(key_value[1]);
 	list->next = NULL;
 	return(list);
+}
+
+void	ft_lstadd_back_m(t_env **alst, t_env *new)
+{
+	t_env *lst;
+
+	lst = *alst;
+	if (*alst == NULL)
+	{
+		*alst = new;
+		return ;
+	}
+	else
+	{
+		while (lst->next)
+			lst = lst->next;
+		lst->next = new;
+	}
 }
 
 t_env	*get_env(char **envp)
@@ -40,14 +51,13 @@ t_env	*get_env(char **envp)
 	t_env	*head;
 	t_env	*current;
 	char	**key_value;
-	int		i;
 
-	current = (t_env *)malloc(sizeof(t_env));
-	head = current;
+	head = NULL;
 	while(*envp)
 	{
 		key_value = split_env(*envp);
-		current->next = create_node(key_value);
+		current = create_node(current, key_value);
+		ft_lstadd_back_m(&head, current);
 		envp++;
 	}
 	return(head);
@@ -57,13 +67,11 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_env	*head;
 	t_env	*env;
-	
+
 	env = get_env(envp);
-	head = env;
-	while(env->next != NULL)
+	while(env != NULL)
 	{
-		printf("%s\n", env->key);
-		printf("%s\n", env->value);
+		printf("%s=%s\n", env->key, env->value);
 		env = env->next;
 	}
 }
