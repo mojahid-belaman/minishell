c2165f2d49390d875d2e68f5a005fa3e09a0a1df
Khaoula Naboussi <knabouss@e2r8p13.1337.ma>
2021-03-09 08:19:25 +0100

    update

--- execution/env.c
+++ execution/env.c
@@ -1,36 +1,39 @@
-#include "../headers/minishell.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knabouss <knabouss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/09 08:16:50 by knabouss          #+#    #+#             */
+/*   Updated: 2021/03/09 08:17:53 by knabouss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef	struct	s_env
-{
-	char	*key;
-	char	*value;
-	struct	s_env *next;
-}				t_env;
+#include "../headers/minishell.h"
 
 char	**split_env(char *line)
 {
-	char **key_value;
+	char	**key_value;
 
 	key_value = ft_split(line, '=');
-	return(key_value);
+	return (key_value);
 }
 
 t_env	*create_node(t_env *node, char **key_value)
 {
 	t_env	*list;
-	int i;
 
-	i = -1;
 	list = (t_env *)malloc(sizeof(t_env));
 	list->key = ft_strdup(key_value[0]);
 	list->value = ft_strdup(key_value[1]);
 	list->next = NULL;
-	return(list);
+	return (list);
 }
 
 void	ft_lstadd_back_m(t_env **alst, t_env *new)
 {
-	t_env *lst;
+	t_env	*lst;
 
 	lst = *alst;
 	if (*alst == NULL)
@@ -53,23 +56,23 @@ t_env	*get_env(char **envp)
 	char	**key_value;
 
 	head = NULL;
-	while(*envp)
+	while (*envp)
 	{
 		key_value = split_env(*envp);
 		current = create_node(current, key_value);
 		ft_lstadd_back_m(&head, current);
 		envp++;
 	}
-	return(head);
+	return (head);
 }
 
-int	main(int argc, char **argv, char **envp)
+int		main(int argc, char **argv, char **envp)
 {
 	t_env	*head;
 	t_env	*env;
 
 	env = get_env(envp);
-	while(env != NULL)
+	while (env != NULL)
 	{
 		printf("%s=%s\n", env->key, env->value);
 		env = env->next;
